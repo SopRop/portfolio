84f06aff13bb2a462744cedbc7fdc90ae39543d0
unknown <sop.ropert@gmail.com>
2018-01-03 19:02:33 +0100

    responsive + mail message

--- mail.php
+++ mail.php
@@ -6,6 +6,6 @@
   $recipient = "sop.ropert@gmail.com";
   $subject = "Demande de contact";
   $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $formcontent, $mailheader) or die("Erreur !");
-  echo "Merci !";
+  mail($recipient, $subject, $formcontent, $mailheader) or die("Oh oh, il me semble qu'il y a un problème avec l'envoi...");
+  echo "Merci ! Le message a bien été envoyé ;)";
 ?>
